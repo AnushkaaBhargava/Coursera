ca8c51dbf949c6dca48cc2e22398312985fdf30d
Anushkaa Bhargava <anushkaab2005@gmail.com>
2026-06-12 00:18:07 +0530

    backend project 2

--- index.js
+++ index.js
@@ -2,11 +2,27 @@ const express=require("express");
 
 const app=express();
 
-app.post('/signup',function(req,res){
+app.post('/user/signup',function(req,res){
+     res.json({
+        message: "Signup endpoint"
+     })
+})
+
+app.post('/user/signin',function(req,res){
+     res.json({
+        message: "Signin endpoint"
+     })
+})
+
+app.post('/purchase',function(req,res){
+    
+})
+
+app.get('/user/purchases',function(req,res){
 
 })
 
-app.post('/login',function(req,res){
+app.get('/courses', function(req,res){
 
 })
 
