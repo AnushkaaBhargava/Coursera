772c45200ebac888baff5e2f855fcc4928686dba
Anushkaa Bhargava <anushkaab2005@gmail.com>
2026-06-12 13:20:54 +0530

    Added routers final

---
+++ routes/admin.js
@@ -0,0 +1,36 @@
+const {Router}=require("express");
+const adminRouter=Router();
+
+adminRouter.post('/signup',function(req,res){
+     res.json({
+        message: "Signup endpoint for admin"
+     })
+})
+
+adminRouter.post('/signin',function(req,res){
+     res.json({
+        message: "Signin endpoint"
+     })
+})
+
+adminRouter.post('/course',function(req,res){
+     res.json({
+        message: "Course created by admin"
+     })
+})
+
+adminRouter.put('/course',function(req,res){
+     res.json({
+        message: "Update"
+     })
+})
+
+adminRouter.get('/course/bulk',function(req,res){
+     res.json({
+        message: "Get my courses"
+     })
+})
+
+module.exports={
+    adminRouter:adminRouter
+}
